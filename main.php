db6e1a078478325ee00cfdc8a4e987940cd3a07e
Ihor <mrsettle1@gmail.com>
2021-08-17 11:57:02 +0300

    pdo and refactiong !notFinished

--- main.php
+++ main.php
@@ -2,19 +2,21 @@
 
 class FileIO
 {
-    public static function createDir($directory){
+    public static function createDir($directory)
+    {
         return mkdir($directory);
     }
 
-    public static function write($fileName ,$txt){
-    try {
-        $myfile = fopen($fileName, "w");
-        fwrite($myfile, $txt);
-    } catch (Exception $ex){
-        echo $ex->getMessage();
-    } finally {
-        fclose($myfile);
-    }
+    public static function write($fileName ,$txt)
+    {
+        try {
+            $myfile = fopen($fileName, "w");
+            fwrite($myfile, $txt);
+        } catch (Exception $ex){
+            echo $ex->getMessage();
+        } finally {
+            fclose($myfile);
+        }
     }
 
     public static function read($file){
@@ -38,12 +40,16 @@ class DatabaseConfig
         'password' => '',
         'databasetype' => '',
         'databasename' => ''
-
     ];
 
     public function __set($key, $value)
     {
         $this->data[$key] = $value;
+    }
+
+    public function __get($key)
+    {
+        return $this->data[$key];
     }
 
     public function __construct(Array $array)
@@ -62,17 +68,34 @@ class DatabaseConnection
 {
     private static $dependentConfig;
     private static $injection_satisied = false;
+    private static $connection;
     public static function init(DatabaseConfig $config)
     {
-
         self::$dependentConfig = $config;
         self::$injection_satisied = true;
-
     }
 
-    public static function getConfig(){
+    public static function getConfig()
+    {
         if(self::$injection_satisied == true){
             return self::$dependentConfig;
+        }
+    }
+
+    public static function conntect2db()
+    {
+        if(self::$injection_satisied){
+            $user = self::$dependentConfig->get('user');
+            $pass = self::$dependentConfig->get('password'); 
+            $host = self::$dependentConfig->get('host');
+            $db_type = self::$dependentConfig->get('db_type');
+            $dbname  = self::$dependentConfig->get('user');
+            $pdo_str = $db_type.":host=".$host.";dbname=".$dbname.";charset=utf8";
+            $pdo = new PDO($pdo_str, $user, $pass);
+            self::$connection = $pdo;
+            return $pdo;
+        } else {
+            return 1;
         }
     }
 }
@@ -82,28 +105,11 @@ class ConfigurationSaver
     public static function MakeDatabaseConfig(DatabaseConfig $config)
     {
         $fileName="db_conf";
-
         $config_json = json_encode(get_object_vars($config));
-        var_dump($config_json);
-
         FileIO::createDir("config");
         $path = "config/".$fileName.".".$config::$id.".conf";
         FileIO::write($path, $config_json);
         return $path;
-
-    }
-}
-
-class Dialog
-{
-    private static $step;
-    private static $databases= array("0" => "Mysql", "1" => "Postgress");
-    
-    public static function create(){
-        echo "Welcome to ICS builder for your WEB project!";
-        echo "Select your database: ";
-        echo "0 - MySql";
-        echo "1 - Postgress";
 
     }
 }
