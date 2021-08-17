7661c2653ec0baebeb74b12ca9cf02d5d592d406
Ihor <mrsettle1@gmail.com>
2021-08-17 12:00:59 +0300

    Staging and cleaning before pdo connection testing, CLEAN ver 1

--- main.php
+++ main.php
@@ -85,11 +85,11 @@ class DatabaseConnection
     public static function conntect2db()
     {
         if(self::$injection_satisied){
-            $user = self::$dependentConfig->get('user');
+            $user = self::$dependentConfig->get('username');
             $pass = self::$dependentConfig->get('password'); 
             $host = self::$dependentConfig->get('host');
-            $db_type = self::$dependentConfig->get('db_type');
-            $dbname  = self::$dependentConfig->get('user');
+            $db_type = self::$dependentConfig->get('databasetype');
+            $dbname  = self::$dependentConfig->get('databasename');
             $pdo_str = $db_type.":host=".$host.";dbname=".$dbname.";charset=utf8";
             $pdo = new PDO($pdo_str, $user, $pass);
             self::$connection = $pdo;
@@ -116,17 +116,9 @@ class ConfigurationSaver
 
 $db_conf = array("databasetype" => "MySql", "databasename"=> "ICS","host"=>"billing.ics.global", "port"=>"3306", "username"=>"root", "password"=>"toor");
 $db_conf_object = new DatabaseConfig($db_conf);
-
-var_dump($db_conf_object);
-die();
 DatabaseConnection::init($db_conf_object);
 $conf = DatabaseConnection::getConfig();
-
+var_dump($conf);
 $file = ConfigurationSaver::MakeDatabaseConfig($conf);
 $dump = FileIO::read($file);
 echo $dump;
-
-var_dump(function(){
-    return "How could I forget? ";
-});
-
